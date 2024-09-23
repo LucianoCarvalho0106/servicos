2c6150fc90e8aa9304effb5e2b67ffff2337b06d
Luciano Carvalho <carvalholuciano232@gmail.com>
2024-09-23 20:05:54 -0300

    conclusão das funcionalidades de altenticação (email e senha e autenticação com o google)

---
+++ config/firebase.ts
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth,GoogleAuthProvider } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_EWfzhxTIrC1jjwORZbHJD0O8q1BdxMg",
+  authDomain: "servicos-13706.firebaseapp.com",
+  projectId: "servicos-13706",
+  storageBucket: "servicos-13706.appspot.com",
+  messagingSenderId: "1047966707151",
+  appId: "1:1047966707151:web:890f5c77a1e331021c76ba"
+};
+
+const app = initializeApp(firebaseConfig);
+ const db = getFirestore(app);
+ const auth = getAuth(app);
+ const provider = new GoogleAuthProvider();
+ export {db,auth,provider};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,64 @@
 
 import Image from 'next/image'
 import Link from 'next/link';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+
+import { provider,auth } from '../../config/firebase';
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+
 
 export default function Home() {
+
+  const [email,setEmail] = useState<string>();
+  const [senha,setSenha] = useState<string>();
+  
+  const router = useRouter();
+
+
+const entrar = (e:React.MouseEvent<HTMLInputElement>)=>{
+    e.preventDefault();
+
+    if (!email || !senha) {
+      alert('Por favor, preencha todos os campos');
+      return;
+    }
+    
+    function loginUser(email:string, senha:string) {
+      
+      signInWithEmailAndPassword(auth, email, senha)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log("Login realizado com sucesso:", user);
+          setEmail("")
+          setSenha("")
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log("Erro ao fazer login:", errorCode, errorMessage);
+          alert("email ou senha inválidos")
+        });
+    }
+    
+    try {
+      loginUser(email!,senha!);
+    } catch (error) {
+      console.log(error)
+    }
+}
+
+
+const loginWithEmail = async () => {
+  try {
+    await signInWithPopup(auth, provider);
+    router.push('/'); // redireciona para a página inicial após o login
+  } catch (error) {
+    console.error(error);
+  }
+};
+
   return (
     
     <div className='flex flex-col items-center justify-center h-screen'>
@@ -20,13 +76,13 @@ export default function Home() {
 
 
       <form className='w-1/3'>
-        <input className='border-2 border-gray-300 outline-none rounded p-1 w-full my-3 tracking-wider' type="text" placeholder='E-mail: ' />
-        <input className='border-2 border-gray-300 outline-none rounded p-1 w-full' type="password" placeholder='Senha: ' />
-        <input className='w-full bg-blue-500 rounded py-2 my-3 text-gray-50 font-bold text-xl cursor-pointer hover:bg-blue-600  duration-200' type="submit" value='Entrar' onClick={e=>e.preventDefault()}/>
+        <input value={email} onChange={e=>setEmail(e.target.value)}  className='border-2 border-gray-300 outline-none rounded p-1 w-full my-3 tracking-wider' type="text" placeholder='E-mail: ' />
+        <input value={senha} onChange={e=>setSenha(e.target.value)} className='border-2 border-gray-300 outline-none rounded p-1 w-full' type="password" placeholder='Senha: ' />
+        <input className='w-full bg-blue-500 rounded py-2 my-3 text-gray-50 font-bold text-xl cursor-pointer hover:bg-blue-600  duration-200' type="submit" value='Entrar' onClick={entrar}/>
       </form>
 
       <Link href="/cadastro" className='hover:text-blue-500 duration-200'>Ainda não tem cadastro? Cadastre-se</Link>
-      <button className='mt-4 cursor-pointer'>
+      <button onClick={loginWithEmail} className='mt-4 cursor-pointer'>
         <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="48px" height="48px"><path fill="#4caf50" d="M45,16.2l-5,2.75l-5,4.75L35,40h7c1.657,0,3-1.343,3-3V16.2z"/><path fill="#1e88e5" d="M3,16.2l3.614,1.71L13,23.7V40H6c-1.657,0-3-1.343-3-3V16.2z"/><polygon fill="#e53935" points="35,11.2 24,19.45 13,11.2 12,17 13,23.7 24,31.95 35,23.7 36,17"/><path fill="#c62828" d="M3,12.298V16.2l10,7.5V11.2L9.876,8.859C9.132,8.301,8.228,8,7.298,8h0C4.924,8,3,9.924,3,12.298z"/><path fill="#fbc02d" d="M45,12.298V16.2l-10,7.5V11.2l3.124-2.341C38.868,8.301,39.772,8,40.702,8h0 C43.076,8,45,9.924,45,12.298z"/></svg>
       </button>
 
